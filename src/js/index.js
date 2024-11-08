c2adaf2d99403c0a7e77324f75197746037d6eb6
JonathanFelippi <jocafr@gmail.com>
2024-11-07 22:13:38 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,16 @@
 const chaveDeApi = "ae33e0f0f12a49b3a7510249240711";
 const botaoDeBusca = document.querySelector(".btn-busca");
 
-botaoDeBusca.addEventListener("click", async () => {
+botaoDeBusca.addEventListener ("click", async () => {
     const cidade = document.getElementById("input-busca").value;
 
-    if(!cidade) return;
+    if (!cidade) return;
 
     const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${chaveDeApi}&q=${cidade}&aqi=no&lang=pt`;
 
     const resposta = await fetch(apiUrl);
 
-    if(resposta.status !== 200) return;
+    if (resposta.status !== 200) return;
 
     const dados = await resposta.json();
 
@@ -19,12 +19,23 @@ botaoDeBusca.addEventListener("click", async () => {
     const condicao = dados.current.condition.text;
     const velocidadeDoVento = dados.current.wind_kph;
     const iconeCondicao = dados.current.condition.icon;
+    const estado = dados.location.region;
+    const pais = dados.location.country;
+    const sensacao = dados.current.feelslike_c;
+    const visibilidade = dados.current.vis_km;
 
-    document.getElementById("cidade").textContent = cidade;
 
-    document.getElementById("temperatura").textContent = `${temperatura} °C`;
-    document.getElementById("umidade").textContent = `${umidade} %`;
-    document.getElementById("condicao").textContent = condicao;
-    document.getElementById("velocidade-do-vento").textContent = `${velocidadeDoVento} km/h`;
-    document.getElementById("icone-condicao").setAttribute("src", iconeCondicao);
+
+document.getElementById("cidade").textContent = cidade;
+document.getElementById("estado").textContent = estado;
+document.getElementById("pais").textContent = pais;
+
+document.getElementById("temperatura").textContent = `${temperatura} °C`;
+document.getElementById("umidade").textContent = `${umidade} %`;
+document.getElementById("condicao").textContent = condicao;
+document.getElementById("velocidade-do-vento").textContent = `${velocidadeDoVento} km/h`;
+document.getElementById("icone-condicao").setAttribute("src", iconeCondicao);
+document.getElementById("sensacao-termica").textContent = `${sensacao} °C`;
+document.getElementById("visibilidade").textContent = `${visibilidade} Km`;
+
 })
